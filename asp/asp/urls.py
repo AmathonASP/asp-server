3e8c2ed5941371afcdb001107158e0255be08075
gustos <jucie15@nate.com>
2018-07-21 06:56:03 +0900

    d

--- asp/asp/urls.py
+++ asp/asp/urls.py
@@ -3,12 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = []
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('streaming.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
