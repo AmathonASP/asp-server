2934ef04c6618ea4db73b766108c9dc9a0a55078
Jangwon <wkddnjset@naver.com>
2018-07-20 22:48:46 +0900

    Settings

--- asp/asp/settings.py
+++ asp/asp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+pxm2subrt_f8+3y+admbi0&!ph-5mqau!qc4okl+6jcxhv3yy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
