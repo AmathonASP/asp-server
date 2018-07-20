42d792e19eb4f8afed83d85e1a2e4ff758d76baf
gustos <jucie15@nate.com>
2018-07-21 06:55:38 +0900

    modify setting

--- asp/asp/settings/common.py
+++ asp/asp/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'streaming.apps.StreamingConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -108,11 +108,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
-STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
--- asp/asp/settings/prod.py
+++ asp/asp/settings/prod.py
@@ -21,3 +21,22 @@ DATABASES = {
         },
     },
 }
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# # AWS Setting
+# AWS_REGION = 'ap-northeast-2c'
+# AWS_STORAGE_BUCKET_NAME = 'asp-audio-storage'
+# AWS_QUERYSTRING_AUTH = False
+# AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
+# AWS_ACCESS_KEY_ID = 'AKIAJR4VHTZROWTFGAUA'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# # #Media Setting
+# # MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+# # MEDIA_ROOT = MEDIA_URL
