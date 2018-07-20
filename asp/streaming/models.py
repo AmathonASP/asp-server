b055ff488ce4b2510991b700102d17de6b57f125
Jangwon <wkddnjset@naver.com>
2018-07-21 00:22:34 +0900

    Create API View

--- asp/streaming/models.py
+++ asp/streaming/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# ------------------------------------------------------------------
+# TableName   : Audio
+# Description : 음원 테이블
+# ------------------------------------------------------------------
 class Audio(models.Model):
-    thumbnail = models.ImageField()
-    artist = models.CharField(default=None, max_length=32, null=True, blank=True)
-    title = models.CharField(default=None, max_length=64, null=True, blank=True)
+    class Meta:
+        verbose_name_plural = "음원 테이블"
+
+    thumbnail = models.ImageField(upload_to="thumbnail/%Y/%m/%d", verbose_name="썸네일")
+    artist = models.CharField(default=None, max_length=32, null=True, blank=True, verbose_name="아티스트")
+    title = models.CharField(default=None, max_length=64, null=True, blank=True, verbose_name="타이틀")
